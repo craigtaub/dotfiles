f5561c96cb0d3ef3f7841e36f403fa1e9d22e12d
Craig Taub <craigtaub@gmail.com>
2017-01-26 11:00:10 +0000

    update to LS colours and help

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -4,8 +4,41 @@ else
   export PS1='%3~$(git_info_for_prompt)%# '
 fi
 
-export LSCOLORS="exfxcxdxbxegedabagacad"
-export CLICOLOR=true
+export LSCOLORS=gaeeee
+# The color designators are as follows:
+# a     black
+# b     red
+# c     green
+# d     brown
+# e     blue
+# f     magenta
+# g     cyan
+# h     light grey
+# A     bold black, usually shows up as dark grey
+# B     bold red
+# C     bold green
+# D     bold brown, usually shows up as yellow
+# E     bold blue
+# F     bold magenta
+# G     bold cyan
+# H     bold light grey; looks like bright white
+# x     default foreground or background
+#
+# The order of the attributes are as follows:
+#
+# 1.   directory
+# 2.   symbolic link
+# 3.   socket
+# 4.   pipe
+# 5.   executable
+# 6.   block special
+# 7.   character special
+# 8.   executable with setuid bit set
+# 9.   executable with setgid bit set
+# 10.  directory writable to others, with sticky bit
+# 11.  directory writable to others, without sticky bit
+# source https://softwaregravy.wordpress.com/2010/10/16/ls-colors-for-mac/
+export CLICOLOR=1
 
 fpath=($ZSH/functions $fpath)
 
